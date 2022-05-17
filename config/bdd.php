5ad57fb729cb31197eeb96940085c13a2e6a1ee3
deveauxsarah <deveauxsarah.dev42@gmail.com>
2022-05-17 15:03:39 +0200

    update db

--- config/bdd.php
+++ config/bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO("mysql:host=141.94.22.233;dbname=DEVEAUX_Projects", 'DEVEAUX', 'Lena1402.');
+    $db = new PDO("mysql:host=141.94.22.233;dbname=DEVEAUX_comparoperator", 'DEVEAUX', 'Lena1402.');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
